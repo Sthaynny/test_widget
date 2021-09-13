c13ba9cc4fa27872df702ff82f758f456792fd2c
isthaynny <igor.nascimento@popcode.com.br>
2021-09-13 20:21:53 -0300

    feat: criando teste de widgets

--- lib/app/my_home_screen.dart
+++ lib/app/my_home_screen.dart
@@ -13,21 +13,23 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ValueListenableBuilder<bool>(
-        valueListenable: stateButton,
-        builder: (context, bool value, child) {
-          return CupertinoButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              stateButton.value = !stateButton.value;
-            },
-            child: Icon(
-              value ? Icons.favorite : Icons.favorite_border,
-              size: value ? 58 : 32,
-              color: value ? Colors.pinkAccent[800] : Colors.grey,
-            ),
-          );
-        },
+      body: Center(
+        child: ValueListenableBuilder<bool>(
+          valueListenable: stateButton,
+          builder: (context, bool value, child) {
+            return CupertinoButton(
+              padding: EdgeInsets.zero,
+              onPressed: () {
+                stateButton.value = !stateButton.value;
+              },
+              child: Icon(
+                value ? Icons.favorite : Icons.favorite_border,
+                size: value ? 88 : 42,
+                color: value ? Colors.pink : Colors.pink[200],
+              ),
+            );
+          },
+        ),
       ),
     );
   }
--- test/my_home_screen_test.dart
+++ test/my_home_screen_test.dart
@@ -1,29 +1,51 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:test_widget/app/my_app.dart';
+import 'package:test_widget/app/my_home_screen.dart';
 
+// Função auxiliar para envolver os widgets a serem testados.
+Widget makeTestable(Widget widget) => MaterialApp(home: widget);
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('testa se a tela esta sendo instanciada',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(makeTestable(MyHomeScreen()));
+    final Finder scaffoldFiner = find.byType(Scaffold);
+    expect(scaffoldFiner, findsOneWidget);
   });
+
+  testWidgets(
+    'Validação do Completa',
+    (WidgetTester tester) async {
+      // pumpWidget() renderiza a UI dado um widget.
+      await tester.pumpWidget(makeTestable(MyHomeScreen()));
+
+      // Finder utilizado para garantir que um elemento do tipo Icon foi renderizado.
+      final Finder iconFinder = find.byType(Icon);
+
+      // Aqui utilizamos nosso Finder junto ao nosso Matcher para garantirmos que temos apenas um Icon sendo renderizado.
+      expect(iconFinder, findsOneWidget);
+
+      /**
+       * É necessario o Widget para fazer validações mais a fundo.
+       * O tester.widget busca o compenente para que possar ser feita algumas 
+       * validações necessarias.
+       */
+      final Icon icon = tester.widget(iconFinder);
+      expect(icon.color, Colors.pink[200]);
+      expect(icon.size, 42);
+      expect(icon.icon, Icons.favorite_border);
+
+      final buttonFinder = find.byType(CupertinoButton);
+
+      expect(buttonFinder, findsOneWidget);
+
+      await tester.longPress(buttonFinder);
+      await tester.pumpAndSettle();
+
+      final Icon iconAfter = tester.widget(iconFinder);
+      expect(iconAfter.color, Colors.pink);
+      expect(iconAfter.size, 88);
+      expect(iconAfter.icon, Icons.favorite);
+    },
+  );
 }
